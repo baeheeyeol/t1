03fcdd4909f08106ec3b834fdfe25884248613f5
z12999941 <z12999941@999941-BAEHY.poscoplantec.co.kr>
2025-03-19 14:23:04 +0900

    8

--- board/src/main/java/com/study/common/file/FileUtils.java
+++ board/src/main/java/com/study/common/file/FileUtils.java
@@ -1,8 +1,10 @@
 package com.study.common.file;
 
 import com.study.domain.file.FileRequest;
+import com.study.domain.file.FileResponse;
 
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -105,5 +107,38 @@ public class FileUtils {
         }
         return dir.getPath();
     }
+    /**
+     * 파일 삭제 (from Disk)
+     * @param files - 삭제할 파일 정보 List
+     */
+    public void deleteFiles(final List<FileResponse> files) {
+        if (CollectionUtils.isEmpty(files)) {
+            return;
+        }
+        for (FileResponse file : files) {
+            String uploadedDate = file.getCreatedDate().toLocalDate().format(DateTimeFormatter.ofPattern("yyMMdd"));
+            deleteFile(uploadedDate, file.getSaveName());
+        }
+    }
 
+    /**
+     * 파일 삭제 (from Disk)
+     * @param addPath - 추가 경로
+     * @param filename - 파일명
+     */
+    private void deleteFile(final String addPath, final String filename) {
+        String filePath = Paths.get(uploadPath, addPath, filename).toString();
+        deleteFile(filePath);
+    }
+
+    /**
+     * 파일 삭제 (from Disk)
+     * @param filePath - 파일 경로
+     */
+    private void deleteFile(final String filePath) {
+        File file = new File(filePath);
+        if (file.exists()) {
+            file.delete();
+        }
+    }
 }
--- board/src/main/java/com/study/domain/file/FileService.java
+++ board/src/main/java/com/study/domain/file/FileService.java
@@ -1,10 +1,13 @@
 package com.study.domain.file;
 
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -23,5 +26,36 @@ public class FileService {
         }
         fileMapper.saveAll(files);
     }
+    /**
+     * 파일 리스트 조회
+     * @param postId - 게시글 번호 (FK)
+     * @return 파일 리스트
+     */
+    public List<FileResponse> findAllFileByPostId(final Long postId) {
+        return fileMapper.findAllByPostId(postId);
+    }
 
+    /**
+     * 파일 리스트 조회
+     * @param ids - PK 리스트
+     * @return 파일 리스트
+     */
+    public List<FileResponse> findAllFileByIds(final List<Long> ids) {
+        if (CollectionUtils.isEmpty(ids)) {
+            return Collections.emptyList();
+        }
+        return fileMapper.findAllByIds(ids);
+    }
+
+    /**
+     * 파일 삭제 (from Database)
+     * @param ids - PK 리스트
+     */
+    @Transactional
+    public void deleteAllFileByIds(final List<Long> ids) {
+        if (CollectionUtils.isEmpty(ids)) {
+            return;
+        }
+        fileMapper.deleteAllByIds(ids);
+    }
 }
